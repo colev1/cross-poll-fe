fc85ee74aa3e9b3cd7e90e28102c39a982f911de
Cole Vanacore <colevanacore@gmail.com>
2019-02-16 13:15:27 -0700

    Add and style loading component

---
+++ components/Loading/Loading.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, ActivityIndicator } from 'react-native';
+
+const Loading = () => {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="black" style={styles.loading} />
+      </View> 
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+})
+
+export default Loading;
--- components/Pet/Pet.js
+++ components/Pet/Pet.js
@@ -1,8 +1,10 @@
 import React  from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, ImageBackground, Image, ActivityIndicator } from 'react-native';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import { hamburgerIcon } from '../../assets/Hamburger_icon.svg.png';
 import APIkey from '../apiKey';
+import Loading from '../Loading/Loading';
+
 import { cleanShelters } from '../helpers/helpers';
 
 export default class Pet extends React.Component {
@@ -32,7 +34,7 @@ export default class Pet extends React.Component {
 
   render() {
     if (!this.props.pet) {
-      return <Text>Loading!</Text>
+      return <Loading />
     } else {
       const { name, breed, age, description, photos, shelterId } = this.props.pet;
       const { shelterName } = this.props;
