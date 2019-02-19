cd8be09100872e3057d162964f571c5d05df2b40
Ashley Levi <Ashplevi@gmail.com>
2019-02-19 14:28:04 -0700

    Add snapshot tests for favorites and favesInfo components

--- components/App/App.test.js
+++ components/App/App.test.js
@@ -4,9 +4,9 @@ import Home from '../Home/Home';
 import renderer from 'react-test-renderer';
 
 
-describe('Card', () => {
+describe('App', () => {
   it('should match the snapshot with all data passed in correctly', () => { 
-    const wrapper = shallow( <Home 
+    const wrapper = shallow( <App 
     />);
 
     expect(wrapper).toMatchSnapshot(); 
