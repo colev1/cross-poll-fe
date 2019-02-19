fa10e54746e3459df929dfb1889113e936d54fe1
Cole Vanacore <colevanacore@gmail.com>
2019-02-18 22:49:43 -0700

    Reset app state

--- components/App/App.js
+++ components/App/App.js
@@ -7,8 +7,8 @@ export default class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      showLogin: false,
-      userAPIToken: "90f3b9ec-2709-48b9-87e0-b2b8f0fcddbb",
+      showLogin: true,
+      userAPIToken: '',
       userZipCode: ''
     }
   }
