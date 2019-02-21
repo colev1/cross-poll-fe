0c825edd5d612dbd436ab9173c9184bdadd79e59
Cole Vanacore <colevanacore@gmail.com>
2019-02-20 21:55:53 -0700

    Add more tests for login

--- components/Favorites/Favorites.test.js
+++ components/Favorites/Favorites.test.js
@@ -1,49 +1,50 @@
 import React from 'react'; 
 import { shallow, mount } from 'enzyme';
-import Favorites from '../Favorites/Favorites';
+import Favorites from './Favorites';
 import renderer from 'react-test-renderer';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native';
+
 
 describe('Favorites', () => {
   let mockFavorites;
   let mockToken;
   let mockCleanedFaves;
+  let wrapper;
+  let mockLoadDelete;
 
   beforeEach(() => {
     mockFavorites = [];
     mockToken = 'a1ag45';
     mockCleanedFaves = [];
-  })
-
-  it('should match the snapshot with all data passed in correctly', () => { 
-
-    const wrapper = shallow( <Favorites 
+    mockLoadDelete = jest.fn()
+    
+    wrapper = shallow( <Favorites 
       fetchFavorites={jest.fn()} 
-      favorites={mockFavorites} userAPIToken={mockToken}
+      favorites={mockFavorites} 
+      userAPIToken={mockToken}
       cleanedFaves={mockCleanedFaves}
       displayFaves={jest.fn()}
       goBack={jest.fn()}
-    />);
+      showInfo={false}
 
+      loadDelete={mockLoadDelete}
+    />);
+  })
+
+  it('should match the snapshot with all data passed in correctly', () => { 
     expect(wrapper).toMatchSnapshot(); 
   })
 
-  describe('goBack', () => {
     it('should update state of showInfo to be false', () => { 
-  
-      const wrapper = shallow( <Favorites 
-        fetchFavorites={jest.fn()} 
-        favorites={mockFavorites} userAPIToken={mockToken}
-        cleanedFaves={mockCleanedFaves}
-        displayFaves={jest.fn()}
-        goBack={jest.fn()}
-      />);
-
-      wrapper.instance().goBack();
-
-      const expected = false
-      
-      expect(wrapper.instance().state.showInfo).toEqual(false);
-    });
-  });
-
+        wrapper.instance().goBack();
+        const expected = false
+        expect(wrapper.instance().state.showInfo).toEqual(expected);
+      });
+    
+      it('should call deleteFavorite on press of delete button', () => {
+          wrapper.find(TouchableOpacity).simulate('press')
+          const instance = wrapper.instance();
+          jest.spyOn(instance, 'deleteFavorite');
+          expect(instance.deleteFavorite).toHaveBeenCalled()
+    })
 });
