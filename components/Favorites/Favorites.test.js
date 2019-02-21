b3f0fa85fe185e495500ed22e95fe4c093551d47
Ashley Levi <Ashplevi@gmail.com>
2019-02-20 21:07:57 -0700

    Tests not passing

--- components/Favorites/Favorites.test.js
+++ components/Favorites/Favorites.test.js
@@ -2,41 +2,36 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Favorites from '../Favorites/Favorites';
 import renderer from 'react-test-renderer';
+import { TouchableOpacity } from 'react-native'; 
 
 describe('Favorites', () => {
   let mockFavorites;
   let mockToken;
   let mockCleanedFaves;
+  let wrapper;
 
   beforeEach(() => {
     mockFavorites = [];
     mockToken = 'a1ag45';
     mockCleanedFaves = [];
-  })
 
-  it('should match the snapshot with all data passed in correctly', () => { 
-
-    const wrapper = shallow( <Favorites 
+    wrapper = shallow( <Favorites 
       fetchFavorites={jest.fn()} 
       favorites={mockFavorites} userAPIToken={mockToken}
       cleanedFaves={mockCleanedFaves}
       displayFaves={jest.fn()}
       goBack={jest.fn()}
     />);
+  })
+
+  it('should match the snapshot with all data passed in correctly', () => { 
 
     expect(wrapper).toMatchSnapshot(); 
+
   })
 
   describe('goBack', () => {
     it('should update state of showInfo to be false', () => { 
-  
-      const wrapper = shallow( <Favorites 
-        fetchFavorites={jest.fn()} 
-        favorites={mockFavorites} userAPIToken={mockToken}
-        cleanedFaves={mockCleanedFaves}
-        displayFaves={jest.fn()}
-        goBack={jest.fn()}
-      />);
 
       wrapper.instance().goBack();
 
@@ -45,5 +40,13 @@ describe('Favorites', () => {
       expect(wrapper.instance().state.showInfo).toEqual(false);
     });
   });
+
+  it.skip('should call deleteFavorite on press of delete button', () => {
+
+    const deleteFavorite = jest.fn()
+    wrapper.find(TouchableOpacity).at(0).simulate('press')
+    expect(deleteFavorite).toHaveBeenCalled()
+    
+  })
 
 });
