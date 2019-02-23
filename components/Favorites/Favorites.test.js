d8795113c2a84ab52e4f02f34701053c877f1927
Cole Vanacore <colevanacore@gmail.com>
2019-02-22 23:32:22 -0700

    Changes to tests

--- components/Favorites/Favorites.test.js
+++ components/Favorites/Favorites.test.js
@@ -11,14 +11,13 @@ describe('Favorites', () => {
   let mockCleanedFaves;
   let wrapper;
   let mockLoadDelete;
-  let mockReturnHome;
 
   beforeEach(() => {
-    mockFavorites = ['pet','pet'];
+    mockFavorites = [];
     mockToken = 'a1ag45';
     mockCleanedFaves = [];
     mockLoadDelete = jest.fn()
-    mockReturnHome =jest.fn()
+    
     wrapper = shallow( <Favorites 
       fetchFavorites={jest.fn()} 
       favorites={mockFavorites} 
@@ -27,7 +26,7 @@ describe('Favorites', () => {
       displayFaves={jest.fn()}
       goBack={jest.fn()}
       showInfo={false}
-      returnHome={mockReturnHome}
+
       loadDelete={mockLoadDelete}
     />);
   })
@@ -36,71 +35,17 @@ describe('Favorites', () => {
     expect(wrapper).toMatchSnapshot(); 
 
   })
-    it('should call get pet on press of button', () => { 
-      let mockCleanedFaves = [
-        {id: 
-         {$t: 0},
-         name: {
-           $t: 'Bob'
-         }
-        },
-        ]
-        let wrapper = shallow( <Favorites 
-          fetchFavorites={jest.fn()} 
-          favorites={mockFavorites} 
-          userAPIToken={mockToken}
-          cleanedFaves={mockCleanedFaves}
-          displayFaves={jest.fn()}
-          goBack={jest.fn()}
-          showInfo={false}
-          loadDelete={mockLoadDelete}
-        />);
 
-        const spy = jest.spyOn(wrapper.instance(), 'getPet')
-        wrapper.instance().forceUpdate()
-        wrapper.find('TouchableOpacity').at(1).simulate('press')
-        expect(spy).toHaveBeenCalled();
-        expect(mockLoadDelete).toHaveBeenCalled();
+    it('should update state of showInfo to be false', () => { 
+        wrapper.instance().goBack();
+        const expected = false
+        expect(wrapper.instance().state.showInfo).toEqual(expected);
       });
-
-      it('should initially have state of show info false', () => {
-        expect(wrapper.instance().state.showInfo).toEqual(false);
-        expect(wrapper.instance().state.loading).toEqual(false);
-      })
-
-      it('should update state on get pet', () => {
-
-      })
     
       it('should call deleteFavorite on press of delete button', () => {
-       let mockCleanedFaves = [
-         {id: 
-          {$t: 0},
-          name: {
-            $t: 'Bob'
-          }
-         },
-         ]
-        wrapper = shallow( <Favorites 
-          fetchFavorites={jest.fn()} 
-          favorites={mockFavorites} 
-          userAPIToken={mockToken}
-          cleanedFaves={mockCleanedFaves}
-          displayFaves={jest.fn()}
-          goBack={jest.fn()}
-          showInfo={false}
-    
-          loadDelete={mockLoadDelete}
-        />);
-
-        const spy = jest.spyOn(wrapper.instance(), 'deleteFavorite')
-        wrapper.find('TouchableOpacity').at(0).simulate('press')
-        expect(spy).toHaveBeenCalled();
-        expect(mockLoadDelete).toHaveBeenCalled();
-    })
-
-    it('should call return home if show info is shown and button is pressed', () => {
-      wrapper.find('TouchableOpacity').at(0).simulate('press')
-      expect(mockReturnHome).toHaveBeenCalled();
+          wrapper.find(TouchableOpacity).simulate('press')
+          const instance = wrapper.instance();
+          jest.spyOn(instance, 'deleteFavorite');
+          expect(instance.deleteFavorite).toHaveBeenCalled()
     })
 });
