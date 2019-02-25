1efff6f3da07bcaa32c60c86f1a3a956db6d83c0
Cole Vanacore <colevanacore@gmail.com>
2019-02-25 15:13:17 -0700

    Fix conflicts

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -105,7 +105,8 @@ export default class Home extends React.Component {
     const url = `http://api.petfinder.com/pet.find?format=json&key=${APIkey}&location=${this.state.userLocation.zip_code}&animal=${selectedAnimal}&size=${selectedSize}&sex=${gender}`
 
     this.fetchAllAnimals(url)
-    this.setState({ showFilter: false })
+    this.setState({ showFilter: false,
+    displayedComponent: 'home' })
   }
 
   fetchShelter = async () => {
